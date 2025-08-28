3336a9386862c812ed693527f48336260aebaa2d
LUCAS KENZO MURAKAMI TADA <23.01221-8@maua.br>
2025-08-28 11:08:15 -0300

    apostila 4.3.7

--- mss/lembretes/index.js
+++ mss/lembretes/index.js
@@ -1,17 +1,24 @@
+const bodyParser = require('body-parser');
 const express = require ('express');
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 const lembretes = {};
+contador = 0;
 
 // definição de endpoints
 //GET no padrão /lembretes
 app.get('/lembretes', (req, res) => {
-    res.send('Lista de lembretes')
+    res.send(lembretes)
 })
 // para criar novo lembrete
 //POST /lembretes {texto: fazer café}
 app.post('/lembretes', (req, res) => {
-    
+    contador++;
+    const {texto} = req.body;
+    lembretes[contador] = {
+        contador, texto
+    }
+    res.status(201).send(lembretes[contador]);
 })
 const port = 4000
 app.listen(port, () => {
