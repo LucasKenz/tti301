30f351213ec8a9b5cb609574875b5293dd26cf2d
Lucas Kenzo Tada <lkmtada@gmail.com>
2025-09-01 09:07:47 -0300

    ap 01

--- mss/lembretes/index.js
+++ mss/lembretes/index.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require ('express');
 const app = express();
 app.use(bodyParser.json());
-const lembretes = {};
+const lembretes = {}; // esse é nosso objeto, ele é cosnt porque SEMPRE APONTA para esse objeto, mesmo que o objeto adicione mais linhas nele
 contador = 0;
 
 // definição de endpoints
@@ -14,12 +14,13 @@ app.get('/lembretes', (req, res) => {
 //POST /lembretes {texto: fazer café}
 app.post('/lembretes', (req, res) => {
     contador++;
-    const {texto} = req.body;
+    const {texto} = req.body // extrai a propriedade texto do corpo da requisição
     lembretes[contador] = {
-        contador, texto
+        contador, texto // constroi o objeto JSON puramente textual
+        // cadastra esse objeto na base da forma descrita
     }
-    res.status(201).send(lembretes[contador]);
-})
+    res.status(201).send(lembretes[contador]); // retorna 201
+}) // post cadastra
 const port = 4000
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`)
