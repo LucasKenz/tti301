a98ec31acfab9b6aaf30e2b651f2d516e2cb24ea
Lucas Kenzo Tada <lkmtada@gmail.com>
2025-09-15 09:14:04 -0300

    explicacao do lab

--- mss/classificacao/index.js
+++ mss/classificacao/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-app.use(express.json());
+app.use(express.json()); // middleware para ler JSON
 const palavraChave = "importante";
 
 const funcoes = {
